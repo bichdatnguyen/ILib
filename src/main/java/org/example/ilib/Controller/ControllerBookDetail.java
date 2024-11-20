5313529fd096be31e4c07cc0f4f43b3a1bd53bdf
Vu Tien Dat <23021524@vnu.edu.vn>
2024-11-20 12:16:11 +0700

    Cap nhat : giao dien Menu va gio hang

--- src/main/java/org/example/ilib/Controller/ControllerBookDetail.java
+++ src/main/java/org/example/ilib/Controller/ControllerBookDetail.java
@@ -3,9 +3,14 @@ package org.example.ilib.Controller;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -22,6 +27,9 @@ public class ControllerBookDetail {
 
     @FXML
     private ImageView thumbnail;
+    @FXML
+    private Button Back;
+
 
     public void setAuthorText(String author) {
         authorText.setText(author);
@@ -93,4 +101,15 @@ public class ControllerBookDetail {
             titleText.setText("No title available.");
         }
     }
+
+    @FXML
+    void BacktoMenu(MouseEvent event) throws IOException {
+        Stage stage = (Stage) Back.getScene().getWindow();
+        FXMLLoader fx = new FXMLLoader();
+        fx.setLocation(getClass().getResource("/org/example/ilib/Menu.fxml"));
+        Scene scene = new Scene(fx.load());
+        stage.setScene(scene);
+
+    }
+
 }
