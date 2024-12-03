dfe7129fc96e1aa37d66c1ec26aca5f0ef1683a3
bichdatnguyen <bichdat05@gmail.com>
2024-12-03 21:15:46 +0700

    update comment

--- src/main/java/org/example/ilib/Controller/ControllerBookDetail.java
+++ src/main/java/org/example/ilib/Controller/ControllerBookDetail.java
@@ -91,10 +91,10 @@ public class ControllerBookDetail {
         titleText.setText(book.getTitle());
         authorText.setText(book.getAuthor());
         String description = book.getDescription();
-        if (description == null ||description.length() < 250 ) {
+        if (description == null || description.length() < 300) {
             descriptionText.setText(description);
         } else {
-            descriptionText.setText(book.getDescription().substring(0, 250));
+            descriptionText.setText(book.getDescription().substring(0, 299));
         }
         idText.setText(book.getId());
         quantityText.setText(String.valueOf(book.getQuantity()));
